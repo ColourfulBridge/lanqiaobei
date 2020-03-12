d0e15f4eafa6fe98f9348b02edab4078683154f0
ColourfulBridge <1160688035@qq.com>
2020-03-12 23:17:35 +0800

    update

--- src/basic/Basic18.java
+++ src/basic/Basic18.java
@@ -1,22 +1,51 @@
 package basic;
+
+import java.util.Scanner;
+
 /**
- * 问题描述
-　　给定一个长度为n的数列，将这个数列按从小到大的顺序排列。1<=n<=200
+ 问题描述
+　　平面上有两个矩形，它们的边平行于直角坐标系的X轴或Y轴。对于每个矩形，我们给出它的一对相对顶点的坐标，请你编程算出两个矩形的交的面积。
 输入格式
-　　第一行为一个整数n。
-　　第二行包含n个整数，为待排序的数，每个整数的绝对值小于10000。
+　　输入仅包含两行，每行描述一个矩形。
+　　在每行中，给出矩形的一对相对顶点的坐标，每个点的坐标都用两个绝对值不超过10^7的实数表示。
 输出格式
-　　输出一行，按从小到大的顺序输出排序后的数列。
+　　输出仅包含一个实数，为交的面积，保留到小数后两位。
 样例输入
-5
-8 3 6 4 9
+1 1 3 3
+2 2 4 4
 样例输出
-3 4 6 8 9
- * @author 张琦
+1.00
  *
  */
 public class Basic18 {
 	public static void main(String[] args) {
+		//点数组
+		double[][] points=new double[2][4];
 		
+		//输入数组
+		Scanner sc=new Scanner(System.in);
+		for(int i=0;i<2;i++)
+		{
+			for(int j=0;j<4;j++)
+				points[i][j]=sc.nextDouble();//输入按格式输
+		}
+		sc.close();
+		
+		//求相交边界
+		//左边界的最大值，右边界的最小值，上边界的最小值，下边界的最大值
+		double left_border=Math.max(Math.min(points[0][0], points[0][2]), Math.min(points[1][0], points[1][2]));
+		double right_border=Math.min(Math.max(points[0][0], points[0][2]), Math.max(points[1][0], points[1][2]));
+		double up_border=Math.min(Math.max(points[0][1], points[0][3]), Math.max(points[1][1], points[1][3]));
+		double down_border=Math.max(Math.min(points[0][1], points[0][3]), Math.min(points[1][1], points[1][3]));
+		
+		//如果左边界小于右边界，或者上边界小于下边界时，相交矩形不存在，也就是两个矩形不相交
+		if(left_border>right_border||up_border<down_border)
+			System.out.println("0.00");
+		else {
+			double length=right_border-left_border;
+			double height=up_border-down_border;
+			System.out.println(String.format("%.2f",length*height));//以保留小数点两位的格式输出
+		}
 	}
 }
+
--- src/basic/Basic19.java
+++ src/basic/Basic19.java
@@ -1,22 +1,72 @@
 package basic;
+
+import java.util.Scanner;
+
 /**
  * 问题描述
-　　给定一个长度为n的数列，将这个数列按从小到大的顺序排列。1<=n<=200
-输入格式
-　　第一行为一个整数n。
-　　第二行包含n个整数，为待排序的数，每个整数的绝对值小于10000。
-输出格式
-　　输出一行，按从小到大的顺序输出排序后的数列。
-样例输入
-5
-8 3 6 4 9
-样例输出
-3 4 6 8 9
- * @author 张琦
+ * 回文串，是一种特殊的字符串，它从左往右读和从右往左读是一样的。小龙龙认为回文串才是完美的。现在给你一个串，它不一定是回文的，请你计算最少的交换次数使得该串变成一个完美的回文串。
+ * 交换的定义是：交换两个相邻的字符 例如mamad 第一次交换 ad : mamda 第二次交换 md : madma 第三次交换 ma : madam
+ * (回文！完美！)
+ *  输入格式 第一行是一个整数N，表示接下来的字符串的长度(N <= 8000) 第二行是一个字符串，长度为N.只包含小写字母 输出格式
+ * 如果可能，输出最少的交换次数。 否则输出Impossible 样例输入 5 mamad 样例输出 3
  *
  */
 public class Basic19 {
 	public static void main(String[] args) {
+		//首先明确相邻交换==左右移动
+		//输入
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		String str=sc.next();
 		
+		//先剔除Impossible的情况
+		//个数为奇数的项>=2的情况
+		int[] arr=new int[26];//26个小写字母的个数
+		for(int i=0;i<n;i++)
+		{
+			arr[str.charAt(i)-'a']++;
+		}
+		//查看是否有超过一个奇数个数项
+		int count=0;
+		for(int i=0;i<26;i++)
+		{
+			if(arr[i]%2!=0)
+				count++;
+		}
+		//如果直接Impossible
+		if(count>1)
+		{
+			System.out.println("Impossible");
+		}
+		else {
+			System.out.println(getCount(str));
+		}
+	}
+	
+	public static int getCount(String str)
+	{
+		//排除了多个奇数个的情况
+		if(str.length()==1||str.length()==0)
+			return 0;
+		
+		//每次对开头的字符匹配移动。把从右边数第一个的这个字符移动到末尾是当下最好的方法。
+		char ch=str.charAt(0);
+		//首先对奇数情况特殊处理
+		StringBuffer sb=new StringBuffer(str);
+		int lastindex=str.lastIndexOf(ch);
+		if(lastindex==0)//也就是开头字符只有一个
+		{
+			//如果遇到单个字符的，这样是最少的：比如smmnn->smnnm->mnsnm
+			int count=sb.length()/2;
+			sb.deleteCharAt(0);
+			return count+getCount(sb.toString());
+		}
+		else {
+			//相当于把该字符移动到最末尾
+			int count=str.length()-1-lastindex;
+			sb.deleteCharAt(lastindex);
+			sb.deleteCharAt(0);
+			return count+getCount(sb.toString());
+		}
 	}
 }
